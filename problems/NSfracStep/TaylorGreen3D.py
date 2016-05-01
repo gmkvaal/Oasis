ca1834d139b8856de824e0a57cba7715c3972f8e
Guttorm <guttorm.kvaal@gmail.com>
2016-05-01 16:32:08 +0200

    nu = 1/1000

--- problems/NSfracStep/TaylorGreen3D.py
+++ problems/NSfracStep/TaylorGreen3D.py
@@ -1,4 +1,4 @@
-å__author__ = "Mikael Mortensen <mikaem@math.uio.no>"
+__author__ = "Mikael Mortensen <mikaem@math.uio.no>"
 __date__ = "2013-06-25"
 __copyright__ = "Copyright (C) 2013 " + __author__
 __license__  = "GNU Lesser GPL version 3 or any later version"
